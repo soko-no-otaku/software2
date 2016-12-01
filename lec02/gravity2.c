d84a30ba3e18f7098bd6405337e205cc869674bf
soko-no-otaku <soko.no.otaku@gmail.com>
2016-12-01 16:15:30 +0900

    実習2

---
+++ lec02/gravity2.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+#define WIDTH 75
+#define HEIGHT 40
+
+const double G = 1.0;  // gravity constant
+
+struct star
+{
+  double m;   // mass
+  double x;   // position_x
+  double y;   // position_y
+  double vx;  // velocity_x
+  double vy;  // velocity_y
+};
+
+struct star stars[] = {
+//  { 1.0, -10.0, 0.0, 0.0, 0.0 },
+//  { 0.5,  10.0, 0.0, 0.2, 0.0 },
+  { 10.0,  0.0, 0.0, 0.0,  0.0 },
+  {  1.0, 10.0, 0.0, 0.0,  1.0 },
+  {  0.1, 20.0, 5.0, 0.2, -0.2 },
+};
+
+const int nstars = sizeof(stars) / sizeof(struct star);
+
+void plot_stars(FILE *fp, const double t)
+{
+  int i;
+  char space[WIDTH][HEIGHT];
+
+  memset(space, ' ', sizeof(space));
+  for (i = 0; i < nstars; i++) {
+    const int x = WIDTH  / 2 + stars[i].x;
+    const int y = HEIGHT / 2 + stars[i].y;
+    if (x < 0 || x >= WIDTH)  continue;
+    if (y < 0 || y >= HEIGHT) continue;
+    char c = 'o';
+    if (stars[i].m >= 1.0) c = 'O';
+    if (stars[i].m == 0.0) c = ' ';
+    space[x][y] = c;
+  }
+
+  int x, y;
+  fprintf(fp, "----------\n");
+  for (y = 0; y < HEIGHT; y++) {
+    for (x = 0; x < WIDTH; x++)
+      fputc(space[x][y], fp);
+    fputc('\n', fp);
+  }
+  fflush(fp);
+
+  printf("t = %5.1f", t);
+  for (i = 0; i < nstars; i++)
+    printf(", stars[%d].(x, y) = (%7.2f, %7.2f)", i, stars[i].x, stars[i].y);
+  printf("\n");
+}
+
+double distance_stars(struct star *a, struct star *b) {
+  return sqrt(pow(a->x - b->x, 2) + pow(a->y - b->y, 2));
+}
+
+int is_neighbor(struct star *a, struct star *b) {
+  const double border = 5.0;
+  return (distance_stars(a, b) < border)? 1 : 0;
+}
+
+void update_velocities(const double dt)
+{
+  int i, j;
+  for (i = 0; i < nstars; i++) {
+    double ax, ay;
+    ax = 0; ay = 0;
+    for (j = 0; j < nstars; j++) {
+      if (!is_neighbor(&stars[i], &stars[j])) {
+        if (stars[i].m > 0.0) {
+          double dr = distance_stars(&stars[i], &stars[j]);
+          ax += G * stars[j].m * (stars[j].x - stars[i].x) / pow(dr, 3);
+          ay += G * stars[j].m * (stars[j].y - stars[i].y) / pow(dr, 3);
+        }
+      }
+    }
+    stars[i].vx += ax * dt;
+    stars[i].vy += ay * dt;
+  }
+}
+
+void update_positions(const double dt)
+{
+  int i;
+  for (i = 0; i < nstars; i++) {
+    stars[i].x += stars[i].vx * dt;
+    stars[i].y += stars[i].vy * dt;
+  }
+}
+
+void fusion_stars() {
+  int i, j;
+  for (i = 0; i < nstars; i++) {
+    for (j = 0; j < i; j++) {
+      if (is_neighbor(&stars[i], &stars[j])) {
+        double M = stars[i].m + stars[j].m;
+        stars[i].vx = (stars[i].m * stars[i].vx + stars[j].m * stars[j].vx) / M;
+        stars[i].vy = (stars[i].m * stars[i].vy + stars[j].m * stars[j].vy) / M;
+        stars[j].vx = 0.0;
+        stars[j].vy = 0.0;
+        stars[i].m = M;
+        stars[j].m = 0.0;
+      }
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  const char *filename = "space.txt";
+  FILE *fp;
+  if ((fp = fopen(filename, "a")) == NULL) {
+    fprintf(stderr, "error: cannot open %s.\n", filename);
+    return 1;
+  }
+
+  double dt = atof(argv[1]);
+  const double stop_time = 400;
+
+  int i;
+  double t;
+  for (i = 0, t = 0; t <= stop_time; i++, t += dt) {
+    update_velocities(dt);
+    update_positions(dt);
+    fusion_stars();
+    if (i % 10 == 0) {
+      plot_stars(fp, t);
+      usleep(200 * 1000);
+    }
+  }
+
+  fclose(fp);
+
+  return 0;
+}
