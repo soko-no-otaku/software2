5651f958431be1d69baab19828164391ec415b88
soko-no-otaku <soko.no.otaku@gmail.com>
2016-12-01 14:21:40 +0900

    課題1

--- lec02/gravity1.c
+++ lec02/gravity1.c
@@ -13,12 +13,15 @@ struct star
 {
   double m;   // mass
   double x;   // position_x
+  double y;   // position_y
   double vx;  // velocity_x
+  double vy;  // velocity_y
 };
 
 struct star stars[] = {
-  { 1.0, -10.0, 0.0 },
-  { 0.5,  10.0, 0.2 }
+  { 10.0,  0.0, 0.0, 0.0,  0.0 },
+  {  1.0, 10.0, 0.0, 0.0,  1.0 },
+  {  0.1, 20.0, 5.0, 0.2, -0.2 },
 };
 
 const int nstars = sizeof(stars) / sizeof(struct star);
@@ -31,7 +34,7 @@ void plot_stars(FILE *fp, const double t)
   memset(space, ' ', sizeof(space));
   for (i = 0; i < nstars; i++) {
     const int x = WIDTH  / 2 + stars[i].x;
-    const int y = HEIGHT / 2;
+    const int y = HEIGHT / 2 + stars[i].y;
     if (x < 0 || x >= WIDTH)  continue;
     if (y < 0 || y >= HEIGHT) continue;
     char c = 'o';
@@ -50,7 +53,7 @@ void plot_stars(FILE *fp, const double t)
 
   printf("t = %5.1f", t);
   for (i = 0; i < nstars; i++)
-    printf(", stars[%d].x = %7.2f", i, stars[i].x);
+    printf(", stars[%d].x = %7.2f, stars[%d].y = %7.2f", i, stars[i].x, i, stars[i].y);
   printf("\n");
 }
 
@@ -58,13 +61,17 @@ void update_velocities(const double dt)
 {
   int i, j;
   for (i = 0; i < nstars; i++) {
-    double ax = 0;
+    double ax, ay;
+    ax = 0; ay = 0;
     for (j = 0; j < nstars; j++) {
       if (j != i) {
-        ax += G * stars[j].m * (stars[j].x - stars[i].x) / pow(fabs(stars[j].x - stars[i].x), 3);
+        double dr = sqrt(pow(stars[j].x - stars[i].x, 2) + pow(stars[j].y - stars[i].y, 2));
+        ax += G * stars[j].m * (stars[j].x - stars[i].x) / pow(dr, 3);
+        ay += G * stars[j].m * (stars[j].y - stars[i].y) / pow(dr, 3);
       }
     }
     stars[i].vx += ax * dt;
+    stars[i].vy += ay * dt;
   }
 }
 
@@ -73,6 +80,7 @@ void update_positions(const double dt)
   int i;
   for (i = 0; i < nstars; i++) {
     stars[i].x += stars[i].vx * dt;
+    stars[i].y += stars[i].vy * dt;
   }
 }
 
