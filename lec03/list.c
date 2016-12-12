cb5cb94b61cc8218c10532afeab66ff9421d297e
soko-no-otaku <soko.no.otaku@gmail.com>
2016-12-12 12:26:23 +0900

    実習1

--- lec03/list.c
+++ lec03/list.c
@@ -5,7 +5,7 @@
 
 #define MAXLEN 1000
 
-struct node 
+struct node
 {
   char *str;
   struct node *next;
@@ -20,7 +20,7 @@ Node *push_front(Node *begin, const char *str)
   char *s = malloc(strlen(str) + 1);
   strcpy(s, str);
   p->str = s;
-  p->next = begin; 
+  p->next = begin;
 
   return p;  // Now the new element is the first element in the list
 }
@@ -61,9 +61,31 @@ Node *push_back(Node *begin, const char *str)
   return begin;
 }
 
+Node *pop_back(Node *begin) {
+  assert(begin != NULL);
+
+  if (begin->next == NULL) {
+    return pop_front(begin);
+  }
+
+  Node *current = begin;
+  Node *prev = current;
+  while (current->next != NULL) {
+    prev = current;
+    current = current->next;
+  }
+
+  free(current->str);
+  free(current);
+
+  prev->next = NULL;
+
+  return begin;
+}
+
 Node *remove_all(Node *begin)
 {
-  while ((begin = pop_front(begin))) 
+  while ((begin = pop_front(begin)))
     ; // Repeat pop_front() until the list becomes empty
   return begin;  // Now, begin is NULL
 }
@@ -75,12 +97,12 @@ int main()
   // Read all lines from stdin and store them in the list
   char buf[MAXLEN];
   while (fgets(buf, MAXLEN, stdin)) {
-    begin = push_front(begin, buf);
-    //begin = push_back(begin, buf); // Try this instead of push_front()
+    //begin = push_front(begin, buf);
+    begin = push_back(begin, buf); // Try this instead of push_front()
   }
 
-  //begin = pop_front(begin);  // What will happen if you do this?
-  //begin = pop_back(begin);   // What will happen if you do this?
+  begin = pop_front(begin);  // What will happen if you do this?
+  begin = pop_back(begin);   // What will happen if you do this?
   //begin = remove_all(begin); // What will happen if you do this?
 
   // Print all the strings stored in the list
@@ -88,6 +110,6 @@ int main()
   for (p = begin; p != NULL; p = p->next) {
     printf("%s", p->str);
   }
-  
+
   return 0;
 }
